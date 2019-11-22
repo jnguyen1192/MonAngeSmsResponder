09d36ef74e207e63d4620227e0b7d082a4682fae
jnguyen1192 <johdu13@hotmail.fr>
2019-11-22 17:19:38 +0100

    #2 Associate actions to each buttons

--- app/src/main/java/com/example/monangesmsresponder/MainActivity.java
+++ app/src/main/java/com/example/monangesmsresponder/MainActivity.java
@@ -8,6 +8,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +30,30 @@ public class MainActivity extends AppCompatActivity {
             String[] permission_list = new String[1];
             permission_list[0] = permission;
             ActivityCompat.requestPermissions(this, permission_list, 1);
+        }
+    }
+
+
+    public void buttonClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_miam:
+                Toast.makeText(this, "Button miam Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_work:
+                Toast.makeText(this, "Button work Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_shopping:
+                Toast.makeText(this, "Button shopping Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_guitar:
+                Toast.makeText(this, "Button guitar Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_workout:
+                Toast.makeText(this, "Button workout Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_meeting:
+                Toast.makeText(this, "Button meeting Clicked", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 
